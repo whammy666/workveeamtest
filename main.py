ec66dd59d32f281f475b41d56e33d29d62772792
vadim <transport.vsm@gmail.com>
2024-06-03 14:48:41 +0200

    second commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import shutil
 import time
 import logging
 import argparse
+import sys
 
 logging.basicConfig(
     level=logging.INFO,
@@ -34,7 +35,6 @@ def get_hashes_of_files_in_folder(folder_path: str) -> dict[str, str]:
 
 
 def synchro_folders(source_folder_path: str, target_folder_path: str) -> None:
-    global target_file_path
     source_file_hashes = get_hashes_of_files_in_folder(source_folder_path)
     target_file_hashes = get_hashes_of_files_in_folder(target_folder_path)
 
@@ -42,7 +42,7 @@ def synchro_folders(source_folder_path: str, target_folder_path: str) -> None:
         source_file_path = os.path.join(source_folder_path, relative_path)
         target_file_path = os.path.join(target_folder_path, relative_path)
 
-        if relative_path not in target_folder_path:
+        if relative_path not in target_file_hashes:
             os.makedirs(os.path.dirname(target_file_path), exist_ok=True)
             shutil.copy2(source_file_path, target_file_path)
             logging.info(f"Copied: {source_file_path} to {target_file_path}")
@@ -52,8 +52,8 @@ def synchro_folders(source_folder_path: str, target_folder_path: str) -> None:
 
     for relative_path in target_file_hashes.keys():
         if relative_path not in source_file_hashes:
-            clone_file_path = os.path.join(target_folder_path, relative_path)
-            os.remove(clone_file_path)
+            target_file_path = os.path.join(target_folder_path, relative_path)
+            os.remove(target_file_path)
             logging.info(f"Deleted: {target_file_path}")
 
 
@@ -61,13 +61,19 @@ def main():
     parser = argparse.ArgumentParser(description="Synchronize two folders.")
     parser.add_argument("source", help="Path to the source folder")
     parser.add_argument("target", help="Path to the target folder")
-    parser.add_argument("-i", "--interval", type=int, default=900, help="Sync interval in seconds (default: 900)")
-
+    parser.add_argument("-i", "--interval", type=int, default=60, help="Sync interval in seconds (default: 60)")
     args = parser.parse_args()
 
     source_folder_path = args.source
     target_folder_path = args.target
     sync_interval = args.interval
+
+    if not os.path.exists(source_folder_path):
+        logging.error(f"Source folder '{source_folder_path}' does not exist.")
+        sys.exit(1)
+    if not os.path.exists(target_folder_path):
+        logging.error(f"Target folder '{target_folder_path}' does not exist.")
+        sys.exit(1)
 
     while True:
         synchro_folders(source_folder_path, target_folder_path)
